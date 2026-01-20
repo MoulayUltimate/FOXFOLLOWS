60b3add8b7ed08c067b312ae605fade20d9a2f94
MoulayUltimate <servicemhamedi2@gmail.com>
2026-01-21 00:59:20 +0100

    Update Meta Pixel ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1214714020682687');
+fbq('init', '850291097771553');
 fbq('track', 'PageView');
 `,
           }}
@@ -68,7 +68,7 @@ fbq('track', 'PageView');
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1214714020682687&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=850291097771553&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
